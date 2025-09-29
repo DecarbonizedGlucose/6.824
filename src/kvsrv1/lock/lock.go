e9c8ab6fedfb495559631cc2b39cb773ecbba113
DecarbonizedGlucose <decglu001@gmail.com>
2025-09-29 18:24:39 +0800

    1

--- src/kvsrv1/lock/lock.go
+++ src/kvsrv1/lock/lock.go
@@ -1,7 +1,8 @@
 package lock
 
 import (
-	"6.5840/kvtest1"
+	"6.5840/kvsrv1/rpc"
+	kvtest "6.5840/kvtest1"
 )
 
 type Lock struct {
@@ -11,6 +12,8 @@ type Lock struct {
 	// MakeLock().
 	ck kvtest.IKVClerk
 	// You may add code here
+	name string
+	id   string
 }
 
 // The tester calls MakeLock() and passes in a k/v clerk; your code can
@@ -21,13 +24,28 @@ type Lock struct {
 func MakeLock(ck kvtest.IKVClerk, l string) *Lock {
 	lk := &Lock{ck: ck}
 	// You may add code here
+	lk.name = l
 	return lk
 }
 
 func (lk *Lock) Acquire() {
 	// Your code here
+	lk.id = kvtest.RandValue(8)
+	for {
+		old, v, err := lk.ck.Get(lk.name)
+		if err == rpc.ErrNoKey || (err == rpc.OK && old == "") {
+			err = lk.ck.Put(lk.name, lk.id, v)
+			if err == rpc.OK {
+				return
+			}
+		}
+	}
 }
 
 func (lk *Lock) Release() {
 	// Your code here
+	owner, v, err := lk.ck.Get(lk.name)
+	if err == rpc.OK && owner == lk.id {
+		_ = lk.ck.Put(lk.name, "", v)
+	}
 }
